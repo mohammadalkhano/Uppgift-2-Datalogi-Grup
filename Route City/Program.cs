ab413af1939779e133d55c38b38f4041ace4eefd
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-09-02 10:39:22 +0200

    Add project files.

---
+++ Route City/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Route_City
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

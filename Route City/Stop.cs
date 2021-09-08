9b370cf3d3a415a4ea236b1ca9d5450d75e2fd2e
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-09-08 09:03:06 +0200

    djecency matrix

---
+++ Route City/Stop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route_City
+{
+    public class Stop
+    {
+
+        public Stop()
+        {
+
+        }
+        // Number of vertices
+        private int vertices;
+        char[] stops = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+        int[,] adjecencyMatrix =
+            {
+                    {0,4,7,0,7,0,0,0,0,0 },
+                    {4,0,3,12,0,0,0,5,0,0},
+                    {7,3,0,0,0,0,4,0,12,0},
+                    {0,12,0,0,0,0,0,7,3,0},
+                    {7,0,0,0,0,3,5,0,0,0 },
+                    {0,0, 0,0,3,0,5,0,0,0},
+                    {0,0,4,0,5,5,0,8,13,8},
+                    {0,5,0,7,0,0,8,0,0,9},
+                    {0,0,12,3,0,0,13,0,0,7},
+                    {0,0,0,0,0,0,8,9,7,0}
+            };
+
+    }
+}
---
+++ TestRoute-City.UnitTests/NodeTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace TestRoute_City.UnitTests
+{
+    [TestFixture]
+    public class NodeTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

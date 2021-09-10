20eb02065c2178ffee3b7055e91b9fa55e0be041
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-09-10 14:26:33 +0200

    Initial Dijxtra's algorithm emplementation

--- Route City/Stop.cs
+++ Route City/Stop.cs
@@ -8,14 +8,18 @@ namespace Route_City
 {
     public class Stop
     {
-
-        public Stop()
+        static void InitializeDefulltValue()
         {
-
+            // Initialize all distances as
+            // INFINITE and stpSet[] as false
+            for (int i = 0; i < vertices; i++)
+            {
+                shortestPaht[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
         }
-        // Number of vertices
-        private int vertices;
-        char[] stops = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+
+        static char[] verticesName = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
         int[,] adjecencyMatrix =
             {
                     {0,4,7,0,7,0,0,0,0,0 },
@@ -29,6 +33,91 @@ namespace Route_City
                     {0,0,12,3,0,0,13,0,0,7},
                     {0,0,0,0,0,0,8,9,7,0}
             };
+        // The output array. dist[i] will hold the shortest distance from src to i
+
+
+        // Number of vertices
+        static int vertices = 10;
+        static int[] shortestPaht = new int[vertices];
+        // sptSet[i] will true if vertex
+        // i is included in shortest path
+        // tree or shortest distance from
+        // src to i is finalized
+        static bool[] sptSet = new bool[vertices];
+
+
+
+        static int minDistance(int[] dist, bool[] sptSet)
+        {
+            // Initialize min value
+            int min = int.MaxValue;
+            int min_index = -1;
+
+            for (int i = 0; i < vertices; i++)
+                if (sptSet[i] == false && dist[i] <= min)
+                {
+                    min = dist[i];
+                    min_index = i;
+                }
+
+            return min_index;
+        }
+
+        // A utility function to print
+        // the constructed distance array
+        static void printShortestPath(int[] dist, int n)
+        {
+            Console.Write("Stops	 Distance from A to  E\n");
+            for (int i = 0; i < vertices; i++)
+                Console.Write(verticesName[i] + " \t\t " + dist[i] + "\n");
+        }
+
+        // Function that implements Dijkstra's
+        // single source shortest path algorithm
+        // for a graph represented using adjacency
+        // matrix representation
+        public static void dijkstra(int[,] graph, int src)
+        {
+
+            InitializeDefulltValue();
+            // Distance of source vertex
+            // from itself is always 0
+            shortestPaht[src] = 0;
+
+            // Find shortest path for all vertices
+            for (int count = 0; count < vertices - 1; count++)
+            {
+                // Pick the minimum distance vertex
+                // from the set of vertices not yet
+                // processed. u is always equal to
+                // src in first iteration.
+                int u = minDistance(shortestPaht, sptSet);
+
+                // Mark the picked vertex as processed
+                sptSet[u] = true;
+
+                // Update dist value of the adjacent
+                // vertices of the picked vertex.
+                for (int v = 0; v < vertices; v++)
+
+                    // Update dist[v] only if is not in
+                    // sptSet, there is an edge from u
+                    // to v, and total weight of path
+                    // from src to v through u is smaller
+                    // than current value of dist[v]
+                    if (!sptSet[v] && graph[u, v] != 0 &&
+                        shortestPaht[u] != int.MaxValue && shortestPaht[u] + graph[u, v] < shortestPaht[v])
+                        shortestPaht[v] = shortestPaht[u] + graph[u, v];
+            }
+
+            // print the constructed distance array
+            printShortestPath(shortestPaht, vertices);
+        }
+
 
     }
+
+    // This code is contributed by ChitraNayal
+
+
 }
